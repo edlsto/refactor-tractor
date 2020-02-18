2c94fbef2ce84a1573943e8c077ea9a4666a4956
Edward Stoner <edwardlstoner@gmail.com>
2020-02-18 16:04:18 -0700

    Create sample data

---
+++ src/data/users-sample.js
@@ -0,0 +1,56 @@
+let users = [
+  {
+    "id": 1,
+    "name": "Saige O'Kon",
+    "pantry": [
+      {
+        "ingredient": 11477,
+        "amount": 1
+      },
+      {
+        "ingredient": 93820,
+        "amount": 1
+      },
+      {
+        "ingredient": 11297,
+        "amount": 3
+      },
+      {
+        "ingredient": 11547,
+        "amount": 5
+      },
+      {
+        "ingredient": 1082047,
+        "amount": 5
+      }
+    ]
+  },
+  {
+    "id": 2,
+    "name": "Ephraim Goyette",
+    "pantry": [
+      {
+        "ingredient": 6150,
+        "amount": 5
+      },
+      {
+        "ingredient": 1032009,
+        "amount": 3
+      },
+      {
+        "ingredient": 11979,
+        "amount": 5
+      },
+      {
+        "ingredient": 1082047,
+        "amount": 1
+      },
+      {
+        "ingredient": 1034053,
+        "amount": 4
+      }
+    ]
+  }
+]
+
+export default users;
