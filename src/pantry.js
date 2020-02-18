affe8c093699ebd5fde4db1112ca795ef9fc04c1
Edward Stoner <edwardlstoner@gmail.com>
2020-02-18 16:02:48 -0700

    Add canCook method

--- src/pantry.js
+++ src/pantry.js
@@ -2,6 +2,29 @@ class Pantry {
   constructor(userIngredients) {
     this.contents = userIngredients;
   }
+
+  canCookMeal(recipe) {
+    let canCook;
+    recipe.ingredients.forEach(recipeIngredient => {
+      let pantryItem = this.contents.find(pantryItem => {
+        return pantryItem.ingredient === recipeIngredient.id
+      });
+      if (!pantryItem || recipeIngredient.quantity.amount > pantryItem.amount) {
+        canCook = false;
+      } else {
+        canCook = true;
+      }
+    })
+    return canCook;
+  }
+
+  cookMeal(recipe) {
+    if (this.canCookMeal(recipe)) {
+      this.contents = this.contents.map(pantryItem => {
+
+      })
+    }
+  }
 }
 
 export default Pantry;
