1c1d0d5f7b1f8493eb38e25c4ee7e27c6f421efd
Edward Stoner <edwardlstoner@gmail.com>
2020-02-18 20:51:19 -0700

    Add method for subtracting from pantry

--- src/pantry.js
+++ src/pantry.js
@@ -4,27 +4,70 @@ class Pantry {
   }
 
   canCookMeal(recipe) {
-    let canCook;
-    recipe.ingredients.forEach(recipeIngredient => {
+    // console.log(this.contents)
+    // console.log(recipe.ingredients)
+    for (let i = 0; i < recipe.ingredients.length; i++) {
       let pantryItem = this.contents.find(pantryItem => {
-        return pantryItem.ingredient === recipeIngredient.id
+        return pantryItem.ingredient === recipe.ingredients[i].id
       });
-      if (!pantryItem || recipeIngredient.quantity.amount > pantryItem.amount) {
-        canCook = false;
-      } else {
-        canCook = true;
+      // if (!pantryItem) {
+      //   console.log('in pantry: no')
+      // } else {
+      //   console.log('in pantry ' + pantryItem.amount)
+      // }
+      // console.log('recipe needs: ' + recipe.ingredients[i].quantity.amount)
+      // If we don't have enough of the ingredient
+      if (!pantryItem || recipe.ingredients[i].quantity.amount > pantryItem.amount) {
+        return false;
       }
-    })
-    return canCook;
+    }
+    return true;
   }
 
   cookMeal(recipe) {
     if (this.canCookMeal(recipe)) {
       this.contents = this.contents.map(pantryItem => {
-
+        recipe.ingredients.forEach(ingredient => {
+          if (ingredient.id === pantryItem.ingredient) {
+            pantryItem.amount -= ingredient.quantity.amount
+          }
+        })
+        return {
+          ingredient: pantryItem.ingredient,
+          amount: pantryItem.amount
+        }
       })
+    } else {
+      let itemsNeeded = recipe.ingredients.filter(recipeIngredient => {
+        let pantryItem = this.contents.find(pantryItem => {
+          return pantryItem.ingredient === recipeIngredient.id
+        });
+        return !pantryItem || recipeIngredient.quantity.amount > pantryItem.amount
+      })
+      itemsNeeded = itemsNeeded.map(item => {
+        let pantryItem = this.contents.find(pantryItem => {
+          return pantryItem.ingredient === item.id
+        });
+        if (!pantryItem) {
+          return {
+            name: item.name,
+            quantityNeeded: item.quantity.amount
+          }
+        } else {
+        return {
+          name: item.name,
+          quantityNeeded: item.quantity.amount - pantryItem.amount
+        }
+      }
+      })
+      return itemsNeeded
+      };
     }
   }
-}
+
+
+
+
+
 
 export default Pantry;
