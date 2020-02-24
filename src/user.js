57cf0721610a63f074af53ea129a8302a647ac9d
Mandy Alford <mandyalford@gmail.com>
2020-02-24 13:10:44 -0700

    update user filterbyfavorites method to work with fetch data

--- src/user.js
+++ src/user.js
@@ -34,8 +34,10 @@ class User {
 
   findFavorites(strgToSrch) {
     return this.favoriteRecipes.filter(recipe => {
+      // console.log(recipe.name.toUpperCase())git commit -m ""
       return recipe.name.includes(strgToSrch)
       || recipe.ingredients.find(ingredient => {
+        console.log(ingredient)
         return ingredient.name.includes(strgToSrch)
       });
     });
