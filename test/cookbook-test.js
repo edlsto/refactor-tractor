51b804926412e74de1f9afe025324208a9caeebc
Edward Stoner <edwardlstoner@gmail.com>
2020-02-25 20:40:03 -0700

    Fix lint errors

--- test/cookbook-test.js
+++ test/cookbook-test.js
@@ -1,73 +1,69 @@
 import {expect} from 'chai';
 
 
-import recipeData from '../src/data/recipes';
-import Cookbook from '../src/cookbook';
 import User from '../src/user';
 
 let user1;
 let recipes = {
   recipeData: [
-{
-id: 595736,
-image: "https://spoonacular.com/recipeImages/595736-556x370.jpg",
-ingredients: [
-{
-id: 20081,
-quantity: {
-amount: 1.5,
-unit: "c"
+    {
+      id: 595736,
+      image: "https://spoonacular.com/recipeImages/595736-556x370.jpg",
+      ingredients: [
+        {
+          id: 20081,
+          quantity: {
+            amount: 1.5,
+            unit: "c"
+          }
+        },
+        {
+          id: 18372,
+          quantity: {
+            amount: 0.5,
+            unit: "tsp"
+          }
+        },
+      ],
+      name: "Loaded Chocolate Chip Pudding Cookie Cups",
+      tags: [
+        "antipasti",
+        "starter",
+        "snack",
+        "appetizer",
+        "antipasto",
+        "hor d'oeuvre"
+      ]
+    },
+    {
+      id: 678353,
+      image: "https://spoonacular.com/recipeImages/678353-556x370.jpg",
+      ingredients: [
+        {
+          id: 1009016,
+          quantity: {
+            amount: 1.5,
+            unit: "cups"
+          }
+        },
+        {
+          id: 9003,
+          quantity: {
+            amount: 2,
+            unit: ""
+          }
+        }
+      ],
+      name: "Maple Dijon Apple Cider Grilled Pork Chops",
+      tags: [
+        "lunch",
+        "main course",
+        "main dish",
+        "dinner"
+      ]
+    }
+  ]
 }
-},
-{
-id: 18372,
-quantity: {
-amount: 0.5,
-unit: "tsp"
-}
-},
-],
-name: "Loaded Chocolate Chip Pudding Cookie Cups",
-tags: [
-"antipasti",
-"starter",
-"snack",
-"appetizer",
-"antipasto",
-"hor d'oeuvre"
-]
-},
-{
-id: 678353,
-image: "https://spoonacular.com/recipeImages/678353-556x370.jpg",
-ingredients: [
-{
-id: 1009016,
-quantity: {
-amount: 1.5,
-unit: "cups"
-}
-},
-{
-id: 9003,
-quantity: {
-amount: 2,
-unit: ""
-}
-}
-],
-name: "Maple Dijon Apple Cider Grilled Pork Chops",
-tags: [
-"lunch",
-"main course",
-"main dish",
-"dinner"
-]
-}
-]
-}
-
-let cookbook;
 
 describe('User', () => {
   beforeEach(() => {
@@ -84,17 +80,33 @@ describe('User', () => {
         'ingredient': 93760,
         'amount': 3
       }],
-      recipes,
-      {ingredientsData: [{id: 1009016, name: "apple cider", estimatedCostInCents: 468}, {id: 9003, name: "apple", estimatedCostInCents: 207}, {id: 93760, name: "Whole Grain Teff Flour", estimatedCostInCents: 539}, {id: 1123, name: "eggs", estimatedCostInCents: 472
-    },{
-id: 20081,
-name: "wheat flour",
-estimatedCostInCents: 142
-},{
-id: 18372,
-name: "bicarbonate of soda",
-estimatedCostInCents: 582
-}]}
+    recipes,
+    {
+      ingredientsData: [
+        {
+          id: 1009016,
+          name: "apple cider",
+          estimatedCostInCents: 468
+        },
+        {
+          id: 9003,
+          name: "apple",
+          estimatedCostInCents: 207
+        },
+        {
+          id: 93760, name: "Whole Grain Teff Flour", estimatedCostInCents: 539}, {id: 1123, name: "eggs", estimatedCostInCents: 472
+        },
+        {
+          id: 20081,
+          name: "wheat flour",
+          estimatedCostInCents: 142
+        }, {
+          id: 18372,
+          name: "bicarbonate of soda",
+          estimatedCostInCents: 582
+        }
+      ]
+    }
     );
   });
 
